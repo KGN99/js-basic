410d959b610b49e4c1f8cddac22807adf8e74349
kgn99 <y3ef9484@gmail.com>
2023-01-05 21:32:11 +0900

    Todo List 웹

--- ToDo-list/script.js
+++ ToDo-list/script.js
@@ -1,4 +1,100 @@
-const checkInput = () => {
-  const inputValue = document.querySelector("#todo-input").ariaValueMax;
-  console.log(inputValue);
+const todoInput = document.querySelector("#todo-input");
+const todoList = document.querySelector("#todo-list");
+
+const savedTodoList = JSON.parse(localStorage.getItem("saved-items"));
+
+const createTodo = (storageData) => {
+  let todoContents = todoInput.value;
+  if (storageData) {
+    todoContents = storageData.contents;
+  }
+  const newLi = document.createElement("li");
+  const newSpan = document.createElement("span");
+  const newBtn = document.createElement("button");
+
+  newBtn.addEventListener("click", () => {
+    newLi.classList.toggle("complete");
+    saveItemsFn();
+  });
+
+  newLi.addEventListener("dblclick", () => {
+    newLi.remove();
+    saveItemsFn();
+  });
+
+  if (storageData?.complete) {
+    newLi.classList.add("complete");
+  }
+
+  newSpan.textContent = todoContents;
+  newLi.appendChild(newBtn);
+  newLi.appendChild(newSpan);
+  todoList.appendChild(newLi);
+  todoInput.value = "";
+  saveItemsFn();
 };
+
+const keyCodeCheck = () => {
+  if (window.event.keyCode === 13 && todoInput.value !== "") {
+    createTodo();
+  }
+};
+
+const deleteAll = () => {
+  const liList = document.querySelectorAll("li");
+  for (let l of liList) {
+    l.remove();
+  }
+  saveItemsFn();
+};
+
+const saveItemsFn = () => {
+  const saveItems = [];
+  for (let i = 0; i < todoList.children.length; i++) {
+    const todoObj = {
+      contents: todoList.children[i].querySelector("span").textContent,
+      complete: todoList.children[i].classList.contains("complete"),
+    };
+    saveItems.push(todoObj);
+  }
+  if (saveItems.length < 1) {
+    localStorage.removeItem("saved-items");
+  } else {
+    localStorage.setItem("saved-items", JSON.stringify(saveItems));
+  }
+};
+
+if (savedTodoList) {
+  for (let x of savedTodoList) {
+    createTodo(x);
+  }
+}
+
+const accessToGeo = (position) => {
+  const positionObj = {
+    latitude: position.coords.latitude,
+    longitude: position.coords.longitude,
+  };
+  console.log(positionObj);
+};
+
+const askForLocation = () => {
+  navigator.geolocation.getCurrentPosition(accessToGeo, (error) => {
+    console.log(error);
+  });
+};
+
+// askForLocation();
+
+const pomiseTest = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("success");
+      // reject("error");
+    }, 2000);
+  });
+};
+
+pomiseTest().then((res) => {
+  console.log(res);
+});
